42fac98ff3fb7d7157917eab20d6826acb0d2ef4
Grupo 4 - Intro Web I Cuatrimestre <40473114+G4-WebCETAV@users.noreply.github.com>
2018-07-12 14:24:23 -0600

    Actualización 1 Luis

--- js/change_site.js
+++ js/change_site.js
@@ -2,6 +2,16 @@ let change_img = document.getElementById('change_img');
 let paramer = getParameterByName('paramer');
 let text_on_p = document.getElementById('text_on_p');
 let param = paramer;
+let arr_text1 = [
+    'http://www.tecnobility.com',
+    'http://equiposespecializados.catedu.es/',
+    'http://www.hosteleriaaccesible.es/',
+    'http://www.cnse.es/',
+    'http://www.nomasabuso.com/',
+    'http://www.fundacioncnse.org/educa/internet_seguro/',
+    'http://www.cocemfe.es/portal/',
+    'http://www.fundacioncnse.org/dgt/'
+]
 let arr_text = [
     'Es el primer medio de comunicación ‘online’ a nivel mundial, independiente y con una actualización constante, especializado en tecnología dirigida a personas con discapacidad y personas mayores. El portal de noticias incorpora en la cabecera la posibilidad de personalizar la portada, de manera que los visitantes pueden elegir el tipo de información sobre el que están más interesados (por tipo de discapacidad: visual, auditiva, física, cognitiva u otras, o para personas mayores) y todas las noticias de portada cambian para mostrar aquellas que interesan al usuario.',
     'El portal de noticias incorpora en la cabecera la posibilidad de personalizar la portada, de manera que los visitantes pueden elegir el tipo de información sobre el que están más interesados (por tipo de discapacidad: visual, auditiva, física, cognitiva u otras, o para personas mayores) y todas las noticias de portada cambian para mostrar aquellas que interesan al usuario.',
@@ -12,7 +22,7 @@ let arr_text = [
     'Esta página web fue adaptada por la Confederación de Personas con Discapacidad Física y Orgánica (COCEMFE), para ser totalmente accesible. En esa página dan información acerca de la integración laboral para las personas con discapacidad. ',
     'Esta página web muestra el reglamento para el examen de licencia de conducir. Lo que hicieron es adaptar la página en si para que las personas sordas pudieran entender el contenido por medio de ilustraciones.'
 ]
-change_img.innerHTML = '<img src="../../img/pag' + param + '.png" alt="Esta imagen muestra una captura de un sitio web, utilizado como portal de noticias para informar a las personas con discapacidead de una manera accesible.">';
+change_img.innerHTML = '<a target="_blank" href="' + arr_text1[param-1] + '"><img src="../../img/pag' + param + '.png" alt="Esta imagen muestra una captura de un sitio web, utilizado como portal de noticias para informar a las personas con discapacidead de una manera accesible."></a>';
 text_on_p.innerHTML = arr_text[param-1];
 function change_l(){
     param--;
@@ -29,42 +39,42 @@ function change_param(param){
     
     switch(param){
         case 1:
-            change_img.innerHTML = '<img src="../../img/pag1.png" alt="Esta imagen muestra una captura de un sitio web, utilizado como portal de noticias para informar a las personas con discapacidead de una manera accesible.">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.tecnobility.com"><img src="../../img/pag1.png" alt="Esta imagen muestra una captura de un sitio web, utilizado como portal de noticias para informar a las personas con discapacidead de una manera accesible."></a>';
             text_on_p.innerHTML = 'Es el primer medio de comunicación ‘online’ a nivel mundial, independiente y con una actualización constante, especializado en tecnología dirigida a personas con discapacidad y personas mayores. El portal de noticias incorpora en la cabecera la posibilidad de personalizar la portada, de manera que los visitantes pueden elegir el tipo de información sobre el que están más interesados (por tipo de discapacidad: visual, auditiva, física, cognitiva u otras, o para personas mayores) y todas las noticias de portada cambian para mostrar aquellas que interesan al usuario.';
             break;
 
         case 2:
-            change_img.innerHTML = '<img src="../../img/pag2.png" alt="Esta imagen muestra una captura de un ditio web, utilizado con un portal de noticias "personalizado" para cada usuario dependiendo de su discapacidad.>';
+            change_img.innerHTML = '<a target="_blank" href="http://equiposespecializados.catedu.es/"><img src="../../img/pag2.png" alt="Esta imagen muestra una captura de un ditio web, utilizado con un portal de noticias "personalizado" para cada usuario dependiendo de su discapacidad.></a>';
             text_on_p.innerHTML = 'El portal de noticias incorpora en la cabecera la posibilidad de personalizar la portada, de manera que los visitantes pueden elegir el tipo de información sobre el que están más interesados (por tipo de discapacidad: visual, auditiva, física, cognitiva u otras, o para personas mayores) y todas las noticias de portada cambian para mostrar aquellas que interesan al usuario.';
             break;
             
         case 3:
-            change_img.innerHTML = '<img src="../../img/pag3.png" alt="Esta imagen muestra la captura de un sitio web que realiza una recopilación de hosteles quer va dirigido a las personas con discapacidad.">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.hosteleriaaccesible.es/"><img src="../../img/pag3.png" alt="Esta imagen muestra la captura de un sitio web que realiza una recopilación de hosteles quer va dirigido a las personas con discapacidad."></a>';
             text_on_p.innerHTML = 'Esta página realiza una recopilación de los establecimientos hosteleros fijándose en criterios de accesibilidad para personas con discapacidad, precios y su oferta gastronómica. Este proyecto, tiene como objetivo facilitar la búsqueda de alojamientos turísticos y restaurantes adaptados a determinadas condiciones de accesibilidad, además de ofrecer al usuario un abanico de establecimientos donde se detalla el menú, tipo de cocina y el precio aproximado.';
             break;
         
         case 4:
-            change_img.innerHTML = '<img src="../../img/pag4.png" alt="Esta imagen muestra la captura de un sitio web que brinda accesibilidad web a personas sordas, por medio de videos en lengua de signos, recursos visuales de aumento de texto y descripciones">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.cnse.es/"><img src="../../img/pag4.png" alt="Esta imagen muestra la captura de un sitio web que brinda accesibilidad web a personas sordas, por medio de videos en lengua de signos, recursos visuales de aumento de texto y descripciones"></a>';
             text_on_p.innerHTML = 'En este caso disponemos de una Web que conjuga dos opciones. Por un lado tenemos un diseño visual como corresponde a las personas sordas; y por otro tenemos accesibilidad en la web. Videos en lengua de signos, recursos visuales de aumento de texto y descripciones hacen que la web sea accesible.';
             break;
             
         case 5:
-            change_img.innerHTML = '<img src="../../img/pag5.png" alt="Esta imagen muestra una captura de un sitio web, que brinda recursos como lectura fácil, con un cuerpo de letra mayor, vídeos sencillos con ilustraciones y audio de fácil comprensión para personas con discapacidad.">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.nomasabuso.com/"><img src="../../img/pag5.png" alt="Esta imagen muestra una captura de un sitio web, que brinda recursos como lectura fácil, con un cuerpo de letra mayor, vídeos sencillos con ilustraciones y audio de fácil comprensión para personas con discapacidad."></a>';
             text_on_p.innerHTML = 'Este sitio se diseñó por un grupo de personas con discapacidad intelectual, donde ellos como que tuvieron  el apoyo de psicólogos y fuerzas de seguridad para obtener un recurso útil y adecuado para sus capacidades. Los contenidos se han creado de acuerdo a los principios de lectura fácil, con un cuerpo de letra mayor, vídeos sencillos con ilustraciones y audio de fácil comprensión.';
             break;
         
         case 6:
-            change_img.innerHTML = '<img src="../../img/pag6.png" alt="Esta imagen muestra una captura de un sitio web que brinda accesibilidad para un uso adecuado en el internet y las redes sociuales para niños sordos.">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.fundacioncnse.org/educa/internet_seguro/"><img src="../../img/pag6.png" alt="Esta imagen muestra una captura de un sitio web que brinda accesibilidad para un uso adecuado en el internet y las redes sociuales para niños sordos."></a>';
             text_on_p.innerHTML = 'Esta página es de la Fundación CNSE que creó una página web para favorecer un uso adecuado de Internet y las redes sociales entre niñas y niños sordos a partir de tercer ciclo de Primaria. El acceso a la Red desde esta edad responde a un fin práctico, como un recurso complementario al aula.';
             break;
             
         case 7:
-            change_img.innerHTML = '<img src="../../img/pag7.png" alt="Esta imagen muestra una captura de un sitio web que brinda información laboral para personas con discapacidad de una manera accesible.">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.cocemfe.es/portal/"><img src="../../img/pag7.png" alt="Esta imagen muestra una captura de un sitio web que brinda información laboral para personas con discapacidad de una manera accesible."></a>';
             text_on_p.innerHTML = 'Esta página web fue adaptada por la Confederación de Personas con Discapacidad Física y Orgánica (COCEMFE), para ser totalmente accesible. En esa página dan información acerca de la integración laboral para las personas con discapacidad. ';
             break;
         
         case 8:
-            change_img.innerHTML = '<img src="../../img/pag8.png" alt="Esta imagen muestra una captura de un sitio web que brinda el reglamento para el examen de licencia de conducir a personas con discapacidad, de una manera accesible.">';
+            change_img.innerHTML = '<a target="_blank" href="http://www.fundacioncnse.org/dgt/"><img src="../../img/pag8.png" alt="Esta imagen muestra una captura de un sitio web que brinda el reglamento para el examen de licencia de conducir a personas con discapacidad, de una manera accesible."></a>';
             text_on_p.innerHTML = 'Esta página web muestra el reglamento para el examen de licencia de conducir. Lo que hicieron es adaptar la página en si para que las personas sordas pudieran entender el contenido por medio de ilustraciones.';
             break;
     }
